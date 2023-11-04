c9e5d7727dda7ea654cf5d43b001c72c0a633086
Hunter Gould <hdgould1515@gmail.com>
2023-11-04 15:56:07 -0400

    Refactored for clarity

--- Elgamel.py
+++ Elgamel.py
@@ -1,55 +1,55 @@
 from Computation import *
 
 
-def elgamel_key_creation(p, b):
+def elgamel_key_creation(prime, prim_root):
     """
     Creates a key using the ElGamel crypto-system.
-    :param p: a prime number
-    :param b: a primitive root mod p
-    :return: Public key; (p, b, y), and private key x in form (p, b, y), x
+    :param prime: a prime number
+    :param prim_root: a primitive root mod prime
+    :return: Public key; (prime, prim_root, pub_key), and private key priv_key in form (prime, prim_root, pub_key), priv_key
     """
 
-    # Generate random x in [1, p-2]
-    x = np.random.random_integers(1, p-2, 1)
+    # Generate random priv_key in [1, prime - 2]
+    priv_key = np.random.random_integers(1, prime - 2, 1)
 
-    # Calculate y = b^x mod p
-    y = square_multiply(b, x, p)
+    # Calculate pub_key = prim_root ^ priv_key mod prime
+    pub_key = square_multiply(prim_root, priv_key, prime)
 
-    # Publish public key (p,b,y) and have value of x for use in later decryption
-    return (p, b, y), x
+    # Publish public key (prime, prim_root, pub_key) and have value of priv_key for use in later decryption
+    return (prime, prim_root, pub_key), priv_key
 
 
-def elgamel_encryption(p, b, y, m):
+def elgamel_encryption(prime, prim_root, pub_key, message):
     """
     Encrypts a message using the ElGamel crypto-system
-    :param p: from public key (p, b, y)
-    :param b: from public key (p, b, y)
-    :param y: from public key (p, b, y)
-    :param m: message to encrypt
-    :return: ciphertext C = (c_1,c_2)
+    :param prime: from public key (prime, prim_root, pub_key)
+    :param prim_root: from public key (prime, prim_root, pub_key)
+    :param pub_key: from public key (prime, prim_root, pub_key)
+    :param message: message to encrypt
+    :return: ciphertext = (cipher1, cipher2)
     """
 
-    # Pick random integer k (range arbitrarily restricted) as ephemeral key
-    k = np.random.random_integers(1, p-1, 1)
+    # Pick random integer ephemeral_key (range arbitrarily restricted) as ephemeral key
+    ephemeral_key = np.random.random_integers(1, prime - 1, 1)
 
-    # Calculate c_1 = b^k mod p, and c_2 = my^k mod p
-    c_1 = square_multiply(b, k, p)
-    c_2 = square_multiply(np.multiply(m, y), k, p)
+    # Calculate cipher1 = prim_root ^ ephemeral_key mod prime, and cipher2 = message * pub_key ^ ephermeral_key mod prime
+    cipher1 = square_multiply(prim_root, ephemeral_key, prime)
+    cipher2 = square_multiply(np.multiply(message, pub_key), ephemeral_key, prime)
 
     # Publish cipher text
-    return c_1, c_2
+    return cipher1, cipher2
 
 
-def elgamel_decryption(c_1, c_2, p, x):
+def elgamel_decryption(cipher1, cipher2, prime, priv_key):
     """
     Decrypts a message using the ElGamel crypto-system
-    :param c_1: from ciphertext C = (c_1, c_2)
-    :param c_2: from ciphertext C = (c_1, c_2)
-    :param p: from public key (p, b, y)
-    :param x: private key x. Assumed to be known for decryption
-    :return: decrypted original message m
+    :param cipher1: from ciphertext = (cipher1, cipher2)
+    :param cipher2: from ciphertext = (cipher1, cipher2)
+    :param prime: from public key (prime, prim_root, pub_key)
+    :param priv_key: private key, assumed to be known for decryption
+    :return: decrypted original message
     """
 
-    # Decrypt message by calculating (c_2) * (c_1)^x inverse mod p
-    # return np.mod(np.multiply(c_2, multiplicative_inverse(square_multiply(c_1,x,p))))
-    return np.mod(np.multiply(c_2, multiplicative_inverse(np.power(c_1, x), p)), p)
+    # Decrypt message by calculating (cipher2) * (cipher1) ^ priv_key inverse mod prime
+    # return np.mod(np.multiply(cipher2, multiplicative_inverse(square_multiply(cipher1, priv_key, prime), prime)))
+    return np.mod(np.multiply(cipher2, multiplicative_inverse(np.power(cipher1, priv_key), prime)), prime)
