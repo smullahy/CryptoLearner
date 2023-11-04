03f34ade5fe7e0af1ea8b6b18313c6265594f6e0
Hunter Gould <hdgould1515@gmail.com>
2023-11-04 18:36:16 -0400

    Pushed faulty code for testing

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from Elgamel import *
+from Digital_Signatures import *
+from Diffie_Hellman import *
+from Encoding_scheme import *
+import sympy as sy
 
-for i in range(10):
-    np.sqrt(i)
-print(2)
+example_message = 1231
+
+p = 7919
+b = 7
+
+message = "Hello"
+ascii_message = convert_to_ascii(message)
+
+encrypted = []
+for char in ascii_message:
+    (_, __, pub_key), priv_key = elgamel_key_creation(p, b)
+    cipher1, cipher2 = elgamel_encryption(p, b,pub_key, char)
+    encrypted.append(((cipher1, cipher2), priv_key))
+
+decrypted = []
+for item in encrypted:
+    elgamel_decryption(item[0][0], item[0][1],p,item[1])
+
+print(decrypted)
