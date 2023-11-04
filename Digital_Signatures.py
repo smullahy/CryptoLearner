6cdf7c38c3ef031127e47531fb94b2b4bca69cf3
Hunter Gould <hdgould1515@gmail.com>
2023-11-04 14:06:19 -0400

    Utilized Computation and generated digital signatures

---
+++ Digital_Signatures.py
@@ -0,0 +1,22 @@
+import numpy as np
+from Computation import *
+
+
+def signature_key_creation(p, q):
+    n = np.multiply(p, q)
+    phi_n = euler_totient(p, q)
+    while np.gcd(v, phi_n) != 1:
+        v = np.random.random_integers(2, phi_n - 1)
+    s = multiplicative_inverse(v, phi_n)
+    return (n, v) , s
+
+
+def sign_message(m, n, s):
+    return square_multiply(m, s, n)
+
+
+def verify_signature(m, s, v, n):
+    if square_multiply(s, v, n) == m:
+        return True
+    else:
+        return False
