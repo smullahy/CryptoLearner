273d7eb5a9c15c9b81ae2c756986502eeb521347
Shane <puckstopper46@gmail.com>
2023-11-04 14:06:59 -0400

    Added euler_totient function

--- Computation.py
+++ Computation.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def square_multiply(base, power, modulus):
-    bits = convert_bits(power)
+    bits = __convert_bits(power)
 
     answer = base
     print(f"Answer: {answer}")
@@ -15,8 +15,26 @@ def square_multiply(base, power, modulus):
     return answer
 
 
-def convert_bits(power):
+def __convert_bits(power):
     return bin(power)[2:]
 
 
-def multiplicitve_inverse()
+def euclidean_algorithm(value1, value2):
+    if value1 >= value2:
+        start_value = value1
+    else:
+        start_value = value2
+
+    np.floor_divide(start_value)
+
+
+def __division_remainder(dividend, divisor):
+    np.floor_divide()
+
+
+def multiplicative_inverse(value1, value2, modulus):
+    euclidean_algorithm(value1, value2) =
+
+
+def euler_totient(p, q):
+    return np.multiply(p - 1, q - 1)
